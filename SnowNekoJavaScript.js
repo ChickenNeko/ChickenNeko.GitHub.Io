67f9ba8578e17bd57f8c9554d9a1005dc4266341
ChickenNeko <50848359+ChickenNeko@users.noreply.github.com>
2020-04-06 11:25:54 -0700

    4/6/2020 - Patch 1.04.2
    Updated, got javascript to work, learn a few new tricks to making functions.

--- SnowNekoJavaScript.js
+++ SnowNekoJavaScript.js
@@ -1,2 +1,13 @@
-var date = new Date();
-document.getElementById("dt").innerHTML = date;
+function dateFunction()
+{
+    var date = new Date();
+    document.getElementById("dt").innerHTML = date.toDateString();
+
+}
+
+window.addEventListener("load", (event) => { i = document.getElementById("para"); i.innerHTML = new Date().toDateString(); })
+
+function myFunction() 
+{
+    alert("hello");
+}
